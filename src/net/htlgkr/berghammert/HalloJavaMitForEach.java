dc66454d13e1674fec291a02af71d82215d3c254
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-09-28 13:07:21 +0200

    Klasse "HalloJavaMitForEach" erstellt

---
+++ src/net/htlgkr/berghammert/HalloJavaMitForEach.java
@@ -0,0 +1,12 @@
+package net.htlgkr.berghammert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavaMitForEach {
+
+
+    public static void main(String[] args) {
+
+    }
+}
