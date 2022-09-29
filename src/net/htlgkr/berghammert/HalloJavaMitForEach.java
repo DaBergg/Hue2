d80ba8c0f8b556c70e2a8a331f2aa87e3a0b4000
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-09-29 16:04:00 +0200

    Eine Liste voller Strings erstellt und mit der 'ForEach' Schleife ausgegeben

--- src/net/htlgkr/berghammert/HalloJavaMitForEach.java
+++ src/net/htlgkr/berghammert/HalloJavaMitForEach.java
@@ -7,6 +7,15 @@ public class HalloJavaMitForEach {
 
 
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<>();
+        list.add("Hallo");
+        list.add("Java");
+        list.add("Mit");
+        list.add("For");
+        list.add("Each");
+        for (String s: list
+             ) {
+            System.out.println(s);
+        }
     }
 }
