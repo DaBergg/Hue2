a669548ce5bc2bbaf0e8197e94e1fe795093f5a7
DaBergg <BerghammerT190006@sus.htl-grieskirchen.at>
2022-09-29 16:22:32 +0200

    In der NumberTester Klasse alle Methoden und Variablen implementiert

--- src/net/htlgkr/berghammert/NumberTester.java
+++ src/net/htlgkr/berghammert/NumberTester.java
@@ -1,4 +1,25 @@
 package net.htlgkr.berghammert;
 
 public class NumberTester {
+    private String filename;
+    NumberTest oddTeser;
+    NumberTest primeTester;
+    NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+        this.filename = fileName;
+    }
+
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTeser = oddTester;
+    }
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+    public void testFile() {
+
+    }
 }
